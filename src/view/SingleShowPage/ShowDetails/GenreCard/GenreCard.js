0d2a8286eb9b8fea3f210421e5bfec6205a37432
djukanovicmiloje <djukanovicmiloje97@gmail.com>
2020-02-13 13:43:47 +0100

    added genere component

---
+++ src/view/SingleShowPage/ShowDetails/GenreCard/GenreCard.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./GenreCard.scss";
+
+const GenreCard = ({ genre }) => <span className="genre__card">{genre}</span>;
+
+export default GenreCard;
--- src/view/SingleShowPage/ShowDetails/ShowDetails.js
+++ src/view/SingleShowPage/ShowDetails/ShowDetails.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./ShowDetails.scss";
+import GenreCard from "./GenreCard/GenreCard";
 
 const ShowDetails = ({ show }) => (
   <div className="show__details">
-    <img src={show.image} alt="show-img" />
-    <h3>{show.name}</h3>
-    {show.genres.map((genre, key) => (
-      <span key={key}>{genre}</span>
-    ))}
+    <div className="image__container">
+      <img src={show.image} alt="show-img" />
+    </div>
+    <div className="info__container">
+      <h3>{show.name}</h3>
+      {show.genres.map((genre, key) => (
+        <GenreCard genre={genre} key={key} />
+      ))}
+    </div>
   </div>
 );
 
